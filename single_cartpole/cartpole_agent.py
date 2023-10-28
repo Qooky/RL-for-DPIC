bbc4e5effbc9fda8a24c36d07c5612a16fe364df
Jadon Duby <jadon.duby@neofinancial.com>
2023-10-28 11:45:54 -0600

    implements Bellman equation. paramaterizes bucket size

--- single_cartpole/cartpole_agent.py
+++ single_cartpole/cartpole_agent.py
@@ -1,23 +1,26 @@
-import gymnasium as gym
+import gym
 import numpy as np
-import matplotlib.pyplot as plt
-import pygame
+import matplotlib.pyplot as pltq
+import time
 
 class CartPoleEnv(gym.Env):
 
     metadata = {"render.modes": ["human", "rgb_array"], "video.frames_per_second": 50}
 
-    def __init__(self, render_mode="", n_observations=1000, q_table=np.array(None)):
+    def __init__(self, render_mode="rgb_array", n_observations=1000, q_table=np.array(None)):
       self.env = gym.make("CartPole-v1", render_mode=render_mode)
       self.action_space = self.env.action_space
       self.observation_space = self.env.observation_space
       self.n_observation_space = 4
-      self.n_buckets = 2
+      self.n_buckets = 16
+      self.bucket_size = self.env.observation_space.high / (self.n_buckets/2)
       self.q_table =  np.zeros((self.env.action_space.n, np.power(self.n_buckets, self.n_observation_space))) if not q_table.all() else q_table
       self.epsilon = 1
       self.n_episodes = n_observations
       self.epsilon_delta = self.epsilon / self.n_episodes
       self.metric = []
+      self.alpha = 0.5
+      self.gamma =  0.99
       return 
     
     def get_index(self, observation):
@@ -27,24 +30,23 @@ class CartPoleEnv(gym.Env):
       for el in observation_bucketed:
         idx = idx + (self.n_buckets**i * el)
         i -= 1
-      return idx
+      return int(idx)
     
     def bucket_observation(self, observation):
-       return (observation < 0).astype(int)
-    
-    def bucket_observation_two(self, observation):
-       return
+      return(np.floor(observation/self.bucket_size))
 
-    def update_q_table(self, observation, action, reward):
-      self.q_table[action][self.get_index(observation)] += reward
+    def update_q_table(self, observation, observation_prime, reward, action=1):
+      Q = self.q_table[action][self.get_index(observation)]
+      Q = (1-self.alpha)*Q + self.alpha*(reward + self.gamma*max(self.q_table[a][self.get_index(observation_prime)] for a in range(2))) 
+      self.q_table[action][self.get_index(observation)] = Q
 
     def train(self):
         observation, _ = self.env.reset()
         j=0
         for i in range(self.n_episodes):
           action = self.policy(observation)
-          observation, reward, terminated, truncated, info = self.env.step(action)
-          self.update_q_table(observation, action, reward)
+          observation_prime, reward, terminated, truncated, info = self.env.step(action)
+          self.update_q_table(observation, observation_prime, reward, action)
           j +=1
           if terminated or truncated:
             self.metric.append(j)
@@ -58,17 +60,16 @@ class CartPoleEnv(gym.Env):
         observation, _ = self.env.reset()
         for i in range(1000):
             index = self.get_index(observation)
-            print("index")
             if self.q_table[0][index] > self.q_table[1][index]:
               action = 0
             else:
               action = 1 
             observation, reward, terminated, truncated, info = self.env.step(action)
             if terminated or truncated:
+              time.sleep(0.1)
               observation, _ = self.env.reset()
         self.env.reset()
         self.env.close()
-        pygame.quit()
             
     def policy(self, observation):
       self.epsilon = self.epsilon - self.epsilon_delta   #at first, low probability to read from q-table, ie high prob take random action
